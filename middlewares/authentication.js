029e5c68f14142dcfd93688af5cf8c862da4534b
Gaurav101 <gauravsingh74580@gmail.com>
2025-02-16 18:31:05 +0530

    First Commit

---
+++ middlewares/authentication.js
@@ -0,0 +1,25 @@
+const { validationToken } = require("../services/authentication");
+
+function checkForAuthenticationCookie(cookieName) {
+    return(req , res , next) => {
+        const tokenCookieValue = req.cookies[cookieName];
+
+        if(!tokenCookieValue){
+            return next();
+        }
+
+        try{
+            const userPayload = validationToken(tokenCookieValue);
+            req.user = userPayload ;
+        }
+        catch (error){}
+
+        return next();
+    };
+}
+
+
+module.exports = {
+    checkForAuthenticationCookie ,
+
+};
